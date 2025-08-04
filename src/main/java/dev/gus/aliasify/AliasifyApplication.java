37db4839126a019d22f446436b02608d0ecab4f2
gustavo <geraldellig@gmail.com>
2025-08-04 15:10:18 -0300

    first commit

---
+++ src/main/java/dev/gus/aliasify/AliasifyApplication.java
@@ -0,0 +1,13 @@
+package dev.gus.aliasify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliasifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliasifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gus/aliasify/AliasifyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gus.aliasify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliasifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
