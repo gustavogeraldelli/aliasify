0c7da27cd6f3eba23276eb0f76f3e676caf1e6c4
gustavo <geraldellig@gmail.com>
2025-08-04 17:12:58 -0300

    feat: add custom methods in repository

--- src/main/java/dev/gus/aliasify/repository/UrlMappingRepository.java
+++ src/main/java/dev/gus/aliasify/repository/UrlMappingRepository.java
@@ -3,5 +3,11 @@ package dev.gus.aliasify.repository;
 import dev.gus.aliasify.entity.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+
+    Optional<UrlMapping> findByOriginalUrl(String originalUrl);
+
+    boolean existsByShortUrl(String shortUrl);
 }
