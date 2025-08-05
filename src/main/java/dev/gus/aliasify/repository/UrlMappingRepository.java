11f2532bd3d70918c8f38a98fd2cdd488854bf78
gustavo <geraldellig@gmail.com>
2025-08-05 15:53:03 -0300

    feat: add query method in repository

--- src/main/java/dev/gus/aliasify/repository/UrlMappingRepository.java
+++ src/main/java/dev/gus/aliasify/repository/UrlMappingRepository.java
@@ -3,6 +3,8 @@ package dev.gus.aliasify.repository;
 import dev.gus.aliasify.entity.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
@@ -12,4 +14,6 @@ public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
     boolean existsByShortUrl(String shortUrl);
 
     Optional<UrlMapping> findByShortUrl(String shortUrl);
+
+    List<UrlMapping> findByExpiresAtBefore(LocalDateTime now);
 }
